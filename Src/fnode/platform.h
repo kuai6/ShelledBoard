50b70c5aa27eb016ed41affc6a975d49c8e917df
Vladislav Volkov <wwwvladislav@gmail.com>
2019-03-16 17:32:19 +0300

    HELO and Conf handlers

---
+++ Src/fnode/platform.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void psleep(uint32_t msec);
+
+#ifdef __cplusplus
+}
+#endif
--- Src/fnode/socket.h
+++ Src/fnode/socket.h
@@ -13,6 +13,13 @@ extern fnet_socket_t const FNET_INVALID_SOCKET;
 
 fnet_socket_t   fnet_socket_create(uint16_t port);
 bool            fnet_socket_sendto(fnet_socket_t sock, const uint8_t *buf, uint16_t len, fnet_address_t const *addr, uint16_t port);
+int32_t         fnet_socket_recvfrom(fnet_socket_t sock,
+                                     uint8_t *buf,
+                                     uint16_t len,
+                                     fnet_address_t *addr,
+                                     uint16_t *port,
+                                     bool block);
+int32_t         fnet_socket_recv(fnet_socket_t sock, uint8_t *buf, uint16_t len, bool block);
 
 #ifdef __cplusplus
 }
